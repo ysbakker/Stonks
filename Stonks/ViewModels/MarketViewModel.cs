fac9b9ff57eedd7e880e66c4611b1f54491004be
Ivo Breukers <ivo@breukers.me>
2021-03-18 14:50:03 +0100

    Implement initial MVVM structure

---
+++ Stonks/ViewModels/MarketViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+
+namespace Stonks
+{
+    public class MarketViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public ObservableCollection<Stock> Stocks { get; set; }
+        public string SearchQuery { get; set; }
+
+        public MarketViewModel()
+        {
+            Stocks = new ObservableCollection<Stock>
+            {
+                new Stock("Dow Jones Industial Average", "DOW J"),
+                new Stock("AEX-INDEX", "^AEX"),
+                new Stock("Apple Inc.", "AAPL"),
+                new Stock("Microsoft Corporation", "MSFT"),
+                new Stock("Tesla, Inc.", "TSLA")
+            };
+        }
+    }
+}
