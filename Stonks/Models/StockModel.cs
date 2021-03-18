83365dd8ddd031f81be7bb6ba12d1eab5fbe21c6
Ivo Breukers <ivo@breukers.me>
2021-03-18 15:29:20 +0100

    Add value and change to stock and try to implement search

---
+++ Stonks/Models/StockModel.cs
@@ -0,0 +1,18 @@
+﻿namespace Stonks
+{
+    public class StockModel
+    {
+        public string Name { get; }
+        public string Symbol { get; }
+        public string Value { get; }
+        public string Change { get; }
+
+        public StockModel(string name, string symbol, string value, string change)
+        {
+            Name = name;
+            Symbol = symbol;
+            Value = value;
+            Change = change;
+        }
+    }
+}
--- Stonks/ViewModels/MarketViewModel.cs
+++ Stonks/ViewModels/MarketViewModel.cs
@@ -6,18 +6,28 @@ namespace Stonks
     public class MarketViewModel : INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler PropertyChanged;
-        public ObservableCollection<Stock> Stocks { get; set; }
-        public string SearchQuery { get; set; }
+        public ObservableCollection<StockModel> Stocks { get; set; }
+        // private string _searchQuery;
+        // public string SearchQuery
+        // {
+        //     get => _searchQuery;
+        //     set
+        //     {
+        //         SearchQuery = value;
+        //         SearchStock(_searchQuery);
+        //         RaisePropertyChanged("SearchQuery");
+        //     }
+        // }
 
         public MarketViewModel()
         {
-            Stocks = new ObservableCollection<Stock>
+            Stocks = new ObservableCollection<StockModel>
             {
-                new Stock("Dow Jones Industial Average", "DOW J"),
-                new Stock("AEX-INDEX", "^AEX"),
-                new Stock("Apple Inc.", "AAPL"),
-                new Stock("Microsoft Corporation", "MSFT"),
-                new Stock("Tesla, Inc.", "TSLA")
+                new StockModel("Dow Jones Industial Average", "DOW J", "33.050,98", "+0,15"),
+                new StockModel("AEX-INDEX", "^AEX", "682,57", "+0,27"),
+                new StockModel("Apple Inc.", "AAPL", "121,70", "-2,45"),
+                new StockModel("Microsoft Corporation", "MSFT", "232,77", "-1,80"),
+                new StockModel("Tesla, Inc.", "TSLA", "677,53", "-3,46")
             };
         }
     }
