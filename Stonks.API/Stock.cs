0d13acdbed128aa3fd662bf2a6fb651d64235158
Bert-Jan <bert-janzwanepol@hotmail.com>
2021-03-19 09:55:40 +0100

    Add Stock Controller and model

---
+++ Stonks.API/Stock.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Stonks.API
+{
+    public class Stock
+    {
+        private string Symbol { get; set; }
+        public string Name { get; set; }
+        public string AssetType { get; set; }
+        public string Description { get; set; }
+        public string Sector { get; set; }
+    }
+}
