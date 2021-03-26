07073b8feaa5d1c4a30dee87bee67052acc79385
Yorrick Bakker <y.bakker@me.com>
2021-03-26 12:07:11 +0100

    Add URL for Android specific

--- Stonks/StonksRestClient/StockMap.cs
+++ Stonks/StonksRestClient/StockMap.cs
@@ -3,13 +3,26 @@ using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
+using Xamarin.Forms;
 
 namespace Stonks.StonksRestClient
 {
     public class StockMap<T>
     {
-        private const string StonksApi = "http://10.0.2.2:3000/stocks";
+        private readonly string StonksApi;
         private readonly HttpClient _httpClient = new HttpClient();
+
+        public StockMap()
+        {
+            if (Device.RuntimePlatform == Device.Android)
+            {
+                StonksApi = "http://10.0.2.2:3000/stocks";
+            }
+            else 
+            {
+                StonksApi = "http://localhost:3000/stocks"; 
+            }
+        }
 
         public async Task<List<T>> GetAllStocks()
         {
