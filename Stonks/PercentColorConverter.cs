a26a3f091381f4402af76bae01a7b167b709d215
Ivo Breukers <ivo@breukers.me>
2021-04-02 10:11:12 +0200

    Update MainPage view

---
+++ Stonks/PercentColorConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace Stonks
+{
+    public class PercentColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value.ToString().Substring(0, 1) == "-" ? "#fe3c30" : "#35c759";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
