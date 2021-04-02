40d713a2b1735410017f150337a4fa92e947ba98
ivobreukers <ism.breukers@gmail.com>
2021-04-02 13:34:36 +0200

    Enable dark mode and implement it

--- Stonks/Assets/PercentColorConverter.cs
+++ Stonks/Assets/PercentColorConverter.cs
@@ -6,9 +6,20 @@ namespace Stonks.Assets
 {
     public class PercentColorConverter : IValueConverter
     {
+        OSAppTheme currentTheme = Application.Current.RequestedTheme;
+
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            return value.ToString().Substring(0, 1) == "-" ? "#fe3c30" : "#35c759";
+            if (value.ToString().Substring(0, 1) == "-")
+                if (currentTheme.Equals(OSAppTheme.Dark))
+                    return "#FE3C2F";
+                else
+                    return "#FE3C30";
+            else
+                if (currentTheme.Equals(OSAppTheme.Dark))
+                    return "#34C759";
+                else
+                    return "#35C759";
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
