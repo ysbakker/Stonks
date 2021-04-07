3745727918a063d2c3b01557c51d14acbf4bad76
Bert-Jan <bert-janzwanepol@hotmail.com>
2021-04-07 14:57:54 +0200

    Remove redundant annotations

--- Stonks/Models/StocksTimeSeriesModel.cs
+++ Stonks/Models/StocksTimeSeriesModel.cs
@@ -7,17 +7,11 @@ namespace Stonks.Models
     {
         [JsonProperty("timeStamp")]
         public string Date { get; set; }
-        [JsonProperty("open")]
         public float Open { get; set; }
-        [JsonProperty("close")]
         public float Close { get; set; }
-        [JsonProperty("high")]
         public float High { get; set; }
-        [JsonProperty("low")]
         public float Low { get; set; }
-        [JsonProperty("volume")]
         public int Volume { get; set; }
-        [JsonProperty("symbol")]
         public string Symbol { get; set; }
     }
 }
