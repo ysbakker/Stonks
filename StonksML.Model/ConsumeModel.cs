c3260362f32c6346a7cb4ee1d7c354e07ce34536
Bert-Jan <bert-janzwanepol@hotmail.com>
2021-04-08 11:13:45 +0200

    Update model location

--- StonksML.Model/ConsumeModel.cs
+++ StonksML.Model/ConsumeModel.cs
@@ -27,7 +27,7 @@ namespace StonksML.Model
             MLContext mlContext = new MLContext();
 
             // Load model & create prediction engine
-            string modelPath = @"C:\Users\Ivo\AppData\Local\Temp\MLVSTools\StonksML\StonksML.Model\MLModel.zip";
+            string modelPath = "./MLModel.zip";
             ITransformer mlModel = mlContext.Model.Load(modelPath, out var modelInputSchema);
             var predEngine = mlContext.Model.CreatePredictionEngine<ModelInput, ModelOutput>(mlModel);
 
